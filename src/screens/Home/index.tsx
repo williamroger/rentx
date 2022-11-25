dfab1c13219945497ccc73872cdd490a291a2f47
William Roger <williamroger.frontend@gmail.com>
2022-11-25 11:49:25 -0300

    adicionado componente Card Car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
 
 import {
   Container,
@@ -12,6 +13,24 @@ import {
 } from './styles';
 
 export function Home() {
+  const carData = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'Ao dia',
+      price: 120,
+    },
+    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png',
+  }
+  const carData2 = {
+    brand: 'Porshe',
+    name: 'Panamera',
+    rent: {
+      period: 'Ao dia',
+      price: 340,
+    },
+    thumbnail: 'https://www.pngkit.com/png/full/237-2375888_porshe-panamera-s.png',
+  }
   return (
     <Container>
       <StatusBar 
@@ -30,6 +49,9 @@ export function Home() {
           </TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carData} />
+      <Car data={carData2} />
     </Container>
   );
 }
