a04a8c236985593c0aee82ed7573ef9511a3fa5f
William Roger <williamroger.frontend@gmail.com>
2022-11-26 23:31:41 -0300

    adicionado componente de acessórios

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
+
+import { Accessory } from '../../components/Accessory';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+
+import speedSvg from '../../assets/speed.svg';
+import accelerationSvg from '../../assets/acceleration.svg';
+import forceSvg from '../../assets/force.svg';
+import gasolineSvg from '../../assets/gasoline.svg';
+import exchangeSvg from '../../assets/exchange.svg';
+import peopleSvg from '../../assets/people.svg';
 
 import {
   Container,
@@ -15,6 +24,7 @@ import {
   Period,
   Price,
   About,
+  Accessories,
 } from './styles';
 
 export function CarDetails() {
@@ -40,6 +50,16 @@ export function CarDetails() {
             <Price>R$ 580</Price>
           </Rent>
         </Details>
+
+        <Accessories>
+          <Accessory name='380Km/h' icon={speedSvg} />
+          <Accessory name='3.2s' icon={accelerationSvg} />
+          <Accessory name='800 HP' icon={forceSvg} />
+          <Accessory name='Gasolina' icon={gasolineSvg} />
+          <Accessory name='Auto' icon={exchangeSvg} />
+          <Accessory name='2 pessoas' icon={peopleSvg} />
+        </Accessories>
+        
         <About>
           Este é um automóvel desportivo. Surgiu do lendário
           touro de lide indultado na praça Real Maestranza de Sevilla.
