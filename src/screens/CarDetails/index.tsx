a031e95d9a72d459b4b188c5ecedf23ee89d36f2
William Roger <williamroger.frontend@gmail.com>
2023-01-04 09:30:34 -0300

    exibindo ícones dinamicamente

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -7,13 +7,6 @@ import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
 import { Button } from '../../components/Button';
 
-import speedSvg from '../../assets/speed.svg';
-import accelerationSvg from '../../assets/acceleration.svg';
-import forceSvg from '../../assets/force.svg';
-import gasolineSvg from '../../assets/gasoline.svg';
-import exchangeSvg from '../../assets/exchange.svg';
-import peopleSvg from '../../assets/people.svg';
-
 import {
   Container,
   Header,
@@ -30,6 +23,7 @@ import {
   Accessories,
   Footer,
 } from './styles';
+import { getAccessoryIcon } from '../../utils/getAccessoryIcon';
 
 interface Params {
   car: CarDTO,
@@ -77,7 +71,7 @@ export function CarDetails() {
               <Accessory 
                 key={accessory.type} 
                 name={accessory.name}
-                icon={speedSvg} 
+                icon={getAccessoryIcon(accessory.type)} 
               />
             ))
           }
